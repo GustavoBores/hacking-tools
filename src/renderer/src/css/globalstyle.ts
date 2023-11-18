a5aff30db5255c07f7cd973c4e7d072b66a8ed96
GustavoBores <binarymasterjobs@gmail.com>
2023-11-17 21:21:05 -0300

    style(global style): added global stylization

---
+++ src/renderer/src/css/globalstyle.ts
@@ -0,0 +1,13 @@
+import { createGlobalStyle } from "styled-components"
+
+const GlobalStyle = createGlobalStyle`
+    * {
+        box-sizing: border-box;
+    }
+
+    html {
+        font-family: "Roboto Mono", monospace;
+    }
+`
+
+export default GlobalStyle
