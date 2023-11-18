f4fde979f037b63fd1997c2d5a7674edbc8a380c
GustavoBores <binarymasterjobs@gmail.com>
2023-11-18 10:18:39 -0300

    style(reset): added css reset

--- src/renderer/src/main.tsx
+++ src/renderer/src/main.tsx
@@ -4,9 +4,11 @@ import ReactDOM from "react-dom/client"
 import App from "./App"
 
 import GlobalStyle from "./css/globalstyle"
+import ResetStyle from "./css/resetstyle"
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
+    <ResetStyle />
     <GlobalStyle />
     <App />
   </React.StrictMode>
